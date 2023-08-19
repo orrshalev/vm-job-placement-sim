4ae5921c0391700f4eeec3e62e9ebd211b9ab8a4
Orr Shalev <shalev.orr@gmail.com>
2023-08-19 19:41:23 -0400

    added flask

--- flaskapi.py
+++ flaskapi.py
@@ -1,5 +1,9 @@
+import os
 import argparse
 import logging
+from time import sleep
+from multiprocessing import Process, Queue
+from flask import jsonify, request, Flask
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -51,3 +55,34 @@ logging.info("FAILURE_TIME: %d", FAILURE_TIME)
 logging.info("COMPLETION_TIME_UPPER: %d", COMPLETION_TIME_UPPER)
 logging.info("COMPLETION_TIME_LOWER: %d", COMPLETION_TIME_LOWER)
 logging.info("EVALUATION_TIME: %d", EVALUATION_TIME)
+
+q = Queue()
+def start_job(seconds: int, q: Queue):
+    sleep(seconds)
+    succesful_count = q.get()
+    q.put(succesful_count + 1)
+q.put(0) # succesful job count
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    succesful_count = q.get()
+    resp = f"Current succesful job count is {succesful_count}" 
+    q.put(succesful_count)
+    return resp
+
+@app.route("/job", methods=["POST"])
+def add_user():
+    json = request.json
+    job_time_s = json["timeSeconds"]
+    if job_time_s and request.method == "POST":
+        Process(target=start_job, args=(job_time_s, q))
+        return jsonify("Job submitted")
+
+    else:
+        return jsonify("Please provide job time")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
